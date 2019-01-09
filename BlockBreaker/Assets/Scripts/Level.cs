5c2a1518edd7429270544d93d47edefd28b8ab2e
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-01-09 00:14:26 -0700

    Sounds and Block Counting

---
+++ BlockBreaker/Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks; // Serialize for Debugging
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+    public void RemoveBreakableBlocks()
+    {
+        breakableBlocks--;
+    }
+}
+
